0aa8b41203a118950755b3dd4df399be8e55d7dc
Malgorzata Salawa <mksalawa@gmail.com>
2018-05-21 13:49:53 +0100

    HTML Unit tests.

---
+++ src/test/java/vvs_webapp/WebappTest.java
@@ -0,0 +1,263 @@
+package vvs_webapp;
+
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.*;
+import com.gargoylesoftware.htmlunit.util.NameValuePair;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class WebappTest {
+
+    private static final String APPLICATION_URL = "http://localhost:8080/VVS_webappdemo/";
+    private static final String[] VALID_VATS = new String[] {
+        "108136701",
+        "108136710",
+        "108136728",
+        "108136736",
+        "108136744",
+        "108136752",
+        "108136779",
+        "108136787",
+        "108136795",
+        "108136809",
+    };
+
+    private static final String CUSTOMER_VAT = "503183504";
+    private static final String CUSTOMER_DESIGNATION = "FCUL";
+    private static final String CUSTOMER_PHONE = "217500000";
+
+    private static HtmlPage page;
+
+    @SuppressWarnings("Duplicates")
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        try (final WebClient webClient = new WebClient(BrowserVersion.getDefault())) {
+    
+            // possible configurations needed to prevent JUnit tests to fail for complex HTML pages
+            webClient.setJavaScriptTimeout(15000);
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+            webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+            webClient.getOptions().setCssEnabled(false);
+            webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+            webClient.getOptions().setJavaScriptEnabled(true);
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+            
+            page = webClient.getPage(APPLICATION_URL);
+            assertEquals(200, page.getWebResponse().getStatusCode()); // OK status
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        addCustomer(CUSTOMER_VAT, CUSTOMER_DESIGNATION, CUSTOMER_PHONE);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        removeCustomer(CUSTOMER_VAT);
+    }
+
+    private HtmlPage removeCustomer(String vat) throws Exception {
+        // at index, goto Remove case and remove the previous client
+        HtmlAnchor removeCustomerLink = page.getAnchorByHref("RemoveCustomerPageController");
+        HtmlPage nextPage = (HtmlPage) removeCustomerLink.openLinkInNewWindow();
+
+        HtmlForm removeCustomerForm = nextPage.getForms().get(0);
+        removeCustomerForm.getInputByName("vat").setValueAttribute(vat);
+        return removeCustomerForm.getInputByName("submit").click();
+    }
+
+    private HtmlPage addCustomer(String vat, String designation, String phone) throws Exception {
+        // get a specific link
+        HtmlAnchor addCustomerLink = page.getAnchorByHref("addCustomer.html");
+        // click on it
+        HtmlPage nextPage = (HtmlPage) addCustomerLink.openLinkInNewWindow();
+        // check if title is the one expected
+        assertEquals("Enter Name", nextPage.getTitleText());
+
+        // get the page first form:
+        HtmlForm addCustomerForm = nextPage.getForms().get(0);
+
+        // place data at form
+        HtmlInput vatInput = addCustomerForm.getInputByName("vat");
+        vatInput.setValueAttribute(vat);
+        HtmlInput designationInput = addCustomerForm.getInputByName("designation");
+        designationInput.setValueAttribute(designation);
+        HtmlInput phoneInput = addCustomerForm.getInputByName("phone");
+        phoneInput.setValueAttribute(phone);
+        // submit form
+        return addCustomerForm.getInputByName("submit").click();
+    }
+
+    /**
+     * After inserting a new address for an existing customer, the table of addresses of that client includes
+     * that address and its total row size increases by one.
+     */
+    @SuppressWarnings("Duplicates")
+    @Test
+    public void insertAddressTest() throws Exception {
+        final String ADDRESS = "address";
+        final String DOOR = "12";
+        final String POSTAL_CODE = "123-456";
+        final String LOCALITY = "locality";
+
+        // check the list of the customer's addresses
+        HtmlPage customerInfoPage = getCustomerInfoPage(CUSTOMER_VAT);
+        List<DomElement> addressListElements = customerInfoPage.getElementsById("address-list");
+        int INITIAL_ROW_COUNT = 0;
+        if (!addressListElements.isEmpty()) {
+            INITIAL_ROW_COUNT = customerInfoPage.<HtmlTable>getHtmlElementById("address-list").getRowCount();
+        }
+
+        // add address to the customer
+        HtmlAnchor addAddressLink = page.getAnchorByHref("addAddressToCustomer.html");
+        HtmlPage nextPage = (HtmlPage) addAddressLink.openLinkInNewWindow();
+        // check if title is the one expected
+        assertEquals("Enter Address", nextPage.getTitleText());
+        // get the page first form:
+        HtmlForm addAddressForm = nextPage.getForms().get(0);
+        // place data in the form
+        addAddressForm.getInputByName("vat").setValueAttribute(String.valueOf(CUSTOMER_VAT));
+        addAddressForm.getInputByName("address").setValueAttribute(ADDRESS);
+        addAddressForm.getInputByName("door").setValueAttribute(DOOR);
+        addAddressForm.getInputByName("postalCode").setValueAttribute(POSTAL_CODE);
+        addAddressForm.getInputByName("locality").setValueAttribute(LOCALITY);
+        // submit form
+        HtmlPage reportPage = addAddressForm.getInputByName("submit").click();
+
+
+        // check if the report page includes the proper values
+        String textReportPage = reportPage.asText();
+        assertTrue(textReportPage.contains(String.valueOf(CUSTOMER_VAT)));
+        assertTrue(textReportPage.contains(ADDRESS));
+        assertTrue(textReportPage.contains(DOOR));
+        assertTrue(textReportPage.contains(POSTAL_CODE));
+        assertTrue(textReportPage.contains(LOCALITY));
+
+        // check the list of the customer's addresses
+        HtmlPage updatedCustomerInfoPage = getCustomerInfoPage(CUSTOMER_VAT);
+
+        HtmlTable addressTable = updatedCustomerInfoPage.getHtmlElementById("address-list");
+        List<HtmlTableRow> rows = addressTable.getRows();
+        assertEquals(INITIAL_ROW_COUNT + 1, rows.size());
+
+        for (HtmlTableRow row : rows) {
+            if (row.getCell(0).asText().equals(ADDRESS) &&
+                row.getCell(1).asText().equals(DOOR) &&
+                row.getCell(2).asText().equals(POSTAL_CODE) &&
+                row.getCell(3).asText().equals(LOCALITY)) {
+                return;
+            }
+        }
+        fail("Could not find the added address.");
+    }
+
+    private HtmlPage getCustomerInfoPage(String vat) throws IOException {
+        HtmlPage customerInfoPage;
+        // Build a GET request
+        try (final WebClient webClient = new WebClient(BrowserVersion.getDefault())) {
+            WebRequest requestSettings = new WebRequest(new URL(APPLICATION_URL + "GetCustomerPageController"), HttpMethod.GET);
+            // Set the request parameters
+            requestSettings.setRequestParameters(new ArrayList<>());
+            requestSettings.getRequestParameters().add(new NameValuePair("vat", vat));
+            customerInfoPage = webClient.getPage(requestSettings);
+        }
+        return customerInfoPage;
+    }
+
+    /**
+     * After inserting new customers, all the information is properly listed in the List All Customers use case.
+     */
+    @Test
+    public void insertNewCustomersTest() throws Exception {
+
+        // make sure the customers to be added are not in the DB
+        removeCustomer(VALID_VATS[0]);
+        removeCustomer(VALID_VATS[1]);
+        removeCustomer(VALID_VATS[2]);
+
+        // get all customers
+        HtmlPage allCustomersPage = (HtmlPage) page.getAnchorByHref("GetAllCustomersPageController").openLinkInNewWindow();
+        final HtmlTable initialCustomers = allCustomersPage.getHtmlElementById("clients");
+
+        String CUSTOMER_0_NAME = "John Snow";
+        String CUSTOMER_1_NAME = "John Brown";
+        String CUSTOMER_2_NAME = "Joe Black";
+        addCustomer(VALID_VATS[0], CUSTOMER_0_NAME, CUSTOMER_PHONE);
+        addCustomer(VALID_VATS[1], CUSTOMER_1_NAME, CUSTOMER_PHONE);
+        addCustomer(VALID_VATS[2], CUSTOMER_2_NAME, CUSTOMER_PHONE);
+
+        // get all customers again
+        HtmlPage updatedAllCustomersPage = (HtmlPage) page.getAnchorByHref("GetAllCustomersPageController").openLinkInNewWindow();
+        final HtmlTable updatedCustomers = updatedAllCustomersPage.getHtmlElementById("clients");
+
+        assertEquals(3 + initialCustomers.getRowCount(), updatedCustomers.getRowCount());
+        List<Integer> foundCustomers = new ArrayList<>();
+        for (HtmlTableRow customerRow : updatedCustomers.getRows()) {
+            checkCustomerRow(0, CUSTOMER_0_NAME, CUSTOMER_PHONE, customerRow, foundCustomers);
+            checkCustomerRow(1, CUSTOMER_1_NAME, CUSTOMER_PHONE, customerRow, foundCustomers);
+            checkCustomerRow(2, CUSTOMER_2_NAME, CUSTOMER_PHONE, customerRow, foundCustomers);
+        }
+        assertEquals("Did not find all added customers.", 3, foundCustomers.size());
+
+        // cleanup - for some reason not working??? caching pages?
+        removeCustomer(VALID_VATS[0]);
+        removeCustomer(VALID_VATS[1]);
+        removeCustomer(VALID_VATS[2]);
+    }
+
+    private void checkCustomerRow(int index, String customerName, String customerPhone, HtmlTableRow customerRow,
+                                  List<Integer> foundCustomers) {
+        if (customerRow.getCell(2).asText().equals(VALID_VATS[index])) {
+            // check if already found
+            if (foundCustomers.contains(index)) {
+                fail("Double entry for vat: " + VALID_VATS[index]);
+            }
+            assertEquals(customerRow.getCell(0).asText(), customerName);
+            assertEquals(customerRow.getCell(1).asText(), customerPhone);
+
+            foundCustomers.add(index);
+        }
+    }
+
+    /**
+     * A new sale is listed as an open sale for the respective customer.
+     */
+    @Test
+    public void newSaleIsOpenTest() throws Exception {
+
+    }
+
+    /**
+     * After closing a sale, it is listed as closed.
+     */
+    @Test
+    public void closeSaleTest() throws Exception {
+
+    }
+
+    /**
+     * After creating a new customer and a new sale for them, inserting a delivery for that sale, the sale delivery
+     * contains all expected information. All intermediate pages have the expected information.
+     */
+    @Test
+    public void saleDeliveryTest() throws Exception {
+
+    }
+
+
+}
+
+
+
+
+
